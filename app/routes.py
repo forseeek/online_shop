2149e6707c81f1b198600d06c86075244e49cef5
forseeek <kirilmitrri4@gmail.com>
2025-12-12 18:30:50 +0100

    homework 07

--- app/routes.py
+++ app/routes.py
@@ -22,17 +22,41 @@ def products():
     return render_template("products_list.html",
                            products=products)
 
+# route for page and request to add a new product
 @bp.route("/add", methods=['GET', 'POST'])
 def add_product():
+    # if method type is post, then add a new product
     if request.method == 'POST':
 
+        # get data from form and save them in variables
         name = request.form['name']
         price = request.form['price']
+        
+        description = request.form['description']
+        stock = request.form['stock']
+        is_active = request.form.get('is_active') == 'on'
+        category = request.form['category']
+        rating = request.form['rating']
+        sale = request.form.get('sale') == 'on'
 
-        product = Product(name=name, price=float(price))
+        # create a new product class
+        product = Product(
+            name=name, 
+            price=float(price),
+            description=description,
+            stock=int(stock),
+            is_active=is_active,
+            category=category,
+            rating=float(rating),
+            sale=sale)
+        # add product to db
         db.session.add(product)
+        # commit changes
         db.session.commit()
 
+        # show message, that product was added
         flash('Product added!')
+        # redirect to products page
         return redirect(url_for('routes.products'))
+    # if method type is get, then render page with variables action and product
     return render_template('product_form.html', action='Add', product=None)
