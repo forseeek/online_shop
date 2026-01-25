bec92b72c9c244d01351e2228032a2345145ab3d
forseeek <kirilmitrri4@gmail.com>
2026-01-25 10:59:32 +0100

    lesson 25.01.26 delete/change product

--- app/routes.py
+++ app/routes.py
@@ -4,59 +4,96 @@ from .models import db, Product
 # create a blueprint class, that allows to organize app routes into separate files
 bp = Blueprint("routes", __name__)
 
+
 # define a route for the main page
 @bp.route("/")
 def index():
     # return the html file for page
     return render_template("index.html")
 
+
 @bp.route("/contacts")
 def contacts():
     return render_template("contacts.html")
+
 
 @bp.route("/products")
 def products():
     # gets products from database
     products = Product.query.all()
     # render product_list.html
-    return render_template("products_list.html",
-                           products=products)
+    return render_template("products_list.html", products=products)
+
 
 # route for page and request to add a new product
-@bp.route("/add", methods=['GET', 'POST'])
+@bp.route("/add", methods=["GET", "POST"])
 def add_product():
     # if method type is post, then add a new product
-    if request.method == 'POST':
+    if request.method == "POST":
 
         # get data from form and save them in variables
-        name = request.form['name']
-        price = request.form['price']
-        
-        description = request.form['description']
-        stock = request.form['stock']
-        is_active = request.form.get('is_active') == 'on'
-        category = request.form['category']
-        rating = request.form['rating']
-        sale = request.form.get('sale') == 'on'
+        name = request.form["name"]
+        price = request.form["price"]
+
+        description = request.form["description"]
+        stock = int(request.form["stock"])
+        is_active = request.form.get("is_active") == "on"
+        category = request.form["category"]
+        rating = float(request.form["rating"])
+        sale = request.form.get("sale") == "on"
+
+        print (f'''[DEBUG]: name={name}, price={price}, description={description}, stock={stock}, is_active={is_active}, category={category}, rating={rating}, sale={sale}''')
 
         # create a new product class
         product = Product(
-            name=name, 
+            name=name,
             price=float(price),
             description=description,
             stock=int(stock),
             is_active=is_active,
             category=category,
             rating=float(rating),
-            sale=sale)
+            sale=sale,
+        )
         # add product to db
         db.session.add(product)
         # commit changes
         db.session.commit()
 
         # show message, that product was added
-        flash('Product added!')
+        flash("Product added!")
         # redirect to products page
-        return redirect(url_for('routes.products'))
+        return redirect(url_for("routes.products"))
     # if method type is get, then render page with variables action and product
-    return render_template('product_form.html', action='Add', product=None)
+    return render_template("product_form.html", action="Add", product=None)
+
+
+@bp.route("/delete/<int:product_id>", methods=["POST"])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    flash("Product deleted!")
+    return redirect(url_for("routes.products"))
+
+
+@bp.route("/update/<int:product_id>", methods=["GET", "POST"])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method == "POST":
+        product.name = request.form["name"]
+        product.price = float(request.form["price"])
+
+        product.description = request.form["description"]
+        product.stock = int(request.form["stock"])
+        product.is_active = request.form.get("is_active") == "on"
+        product.category = request.form["category"]
+        product.rating = float(request.form["rating"])
+        product.sale = request.form.get("sale") == "on"
+
+        print (f'''[DEBUG]: name={product.name}, price={product.price}, description={product.description}, stock={product.stock}, is_active={product.is_active}, category={product.category}, rating={product.rating}, sale={product.sale}''')
+
+        db.session.commit()
+        flash("Product updated!")
+        return redirect(url_for("routes.products"))
+    return render_template("product_form.html", action="Update", product=product)
